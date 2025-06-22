58f79c4e2c9c5297e8086dd0fb667bc823d10fc2
Ecoyle33 <euancoyle111@gmail.com>
2025-06-22 16:57:46 +0100

    Added simple GET request implementation

--- src/main/java/com/example/rockpaperscissors/ScoreController.java
+++ src/main/java/com/example/rockpaperscissors/ScoreController.java
@@ -8,8 +8,30 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class ScoreController {
 
+    static Score score = new Score(30, 20, 10);
+
     @GetMapping("/health-check")
     public String getHealthCheck(){
         return "All gooooood";
+    }
+
+    @GetMapping("/score")
+    public Score getScore(){
+        return score;
+    }
+
+    @GetMapping("/score/wins")
+    public int getWins(){
+        return score.getWins();
+    }
+
+    @GetMapping("/score/losses")
+    public int getLosses(){
+        return score.getLosses();
+    }
+
+    @GetMapping("/score/ties")
+    public int getTies(){
+        return score.getTies();
     }
 }
